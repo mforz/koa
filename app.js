740ef30c40cabf8c6723d2a21164641a07b7fa38
张永生 <zhang.yongsheng@huadongmedia.com>
2018-11-28 15:50:18 +0800

    zys

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ const app = new Koa();
 
 // demos/04.js
 
-// const main = ctx => {
-//   ctx.response.type = 'html';
-//   ctx.response.body = fs.createReadStream('./src/pages/demo.html');
-// };
-
 const main = ctx => {
-    if (ctx.request.path !== '/') {
-      ctx.response.type = 'html';
-      ctx.response.body = '<a href="/">Index Page</a>';
-    } else {
-      ctx.response.body = 'Hello World';
-    }
-  };
+  ctx.response.type = 'html';
+  ctx.response.body = fs.createReadStream('./src/pages/demo.html');
+};
+
+// const main = ctx => {
+//     if (ctx.request.path !== '/') {
+//       ctx.response.type = 'html';
+//       ctx.response.body = '<a href="/">Index Page</a>';
+//     } else {
+//       ctx.response.body = 'Hello World';
+//     }
+//   };
 
 
 // response
